27e3e6391ef7a23ac3bc57aeb6be9f96cf5dd840
vinessa <vinessa2323@gmail.com>
2022-11-07 14:48:26 +0800

    commented main app

--- CaRMSReservationClient/src/java/carmsreservationclient/MainApp.java
+++ CaRMSReservationClient/src/java/carmsreservationclient/MainApp.java
@@ -32,34 +32,35 @@ public class MainApp {
     
     public void runApp()
     {
-        Scanner scanner = new Scanner(System.in);
-        Integer response = 0;
-        
-        while(true)
-        {
-            System.out.println("*** Welcome to Merlion Car Rental ***\n");
-            
-            if(currentCustomer != null)
-            {
-                System.out.println("You are login as " + currentCustomer.getName() + "\n");
-            }
-            //can only login/sign up if the customer is currently NOT logged in
-            else
-            {            
-                System.out.println("1: Sign Up");
-                System.out.println("2: Login");
-            }
-            
-            System.out.println("3: Search Car");
-            System.out.println("4: Exit\n");
-            response = 0;
-            
-            //if answer not valid, then prompt user again
-            while(response < 1 || response > 4)
-            {
-                System.out.print("> ");
-
-                response = scanner.nextInt();
-    
+//        Scanner scanner = new Scanner(System.in);
+//        Integer response = 0;
+//        
+//        while(true)
+//        {
+//            System.out.println("*** Welcome to Merlion Car Rental ***\n");
+//            
+//            if(currentCustomer != null)
+//            {
+//                System.out.println("You are login as " + currentCustomer.getName() + "\n");
+//            }
+//            //can only login/sign up if the customer is currently NOT logged in
+//            else
+//            {            
+//                System.out.println("1: Sign Up");
+//                System.out.println("2: Login");
+//            }
+//            
+//            System.out.println("3: Search Car");
+//            System.out.println("4: Exit\n");
+//            response = 0;
+//            
+//            //if answer not valid, then prompt user again
+//            while(response < 1 || response > 4)
+//            {
+//                System.out.print("> ");
+//
+//                response = scanner.nextInt();
+//    
+    }
 
 }
