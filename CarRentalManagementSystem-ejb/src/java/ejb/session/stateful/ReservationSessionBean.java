db80a3c1810f71548d31ae6880cd53c2d436daff
vinessa <vinessa2323@gmail.com>
2022-11-08 14:02:31 +0800

    search car usecase done

--- CarRentalManagementSystem-ejb/src/java/ejb/session/stateful/ReservationSessionBean.java
+++ CarRentalManagementSystem-ejb/src/java/ejb/session/stateful/ReservationSessionBean.java
@@ -5,9 +5,22 @@
  */
 package ejb.session.stateful;
 
+import ejb.session.stateless.CategorySessionBeanLocal;
+import ejb.session.stateless.RentalRateSessionBeanLocal;
+import entity.Category;
+import entity.Outlet;
+import entity.RentalRate;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.util.Pair;
+import javax.ejb.EJB;
 import javax.ejb.Stateful;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import util.exception.RentalRateNotFoundException;
 
 /**
  *
@@ -16,11 +29,32 @@ import javax.persistence.PersistenceContext;
 @Stateful
 public class ReservationSessionBean implements ReservationSessionBeanRemote, ReservationSessionBeanLocal {
 
+    @EJB(name = "RentalRateSessionBeanLocal")
+    private RentalRateSessionBeanLocal rentalRateSessionBeanLocal;
+
+    @EJB(name = "CategorySessionBeanLocal")
+    private CategorySessionBeanLocal categorySessionBeanLocal;
+    
     @PersistenceContext(unitName = "CarRentalManagementSystem-ejbPU")
     private EntityManager em;
 
     
-
-    // Add business logic below. (Right-click in editor and choose
-    // "Insert Code > Add Business Method")
+    public List<Pair<Category, List<RentalRate>>> searchCar(Category category, Date start, Date end, Outlet pickupOutlet, Outlet returnOutlet) {
+        List<Category> categories = categorySessionBeanLocal.categoriesAvailableForThisPeriod(pickupOutlet, start, end);
+        List<Pair<Category, List<RentalRate>>> res = new ArrayList<>();
+        for(Category c : categories) {
+            try {
+                List<RentalRate> r = rentalRateSessionBeanLocal.calculateRentalRate(c, start, end);
+                Pair<Category, List<RentalRate>> pair = new Pair<Category, List<RentalRate>>(c, r);
+                res.add(pair);
+            } catch (RentalRateNotFoundException ex) {
+                List<RentalRate> r = new ArrayList<>();
+                Pair<Category, List<RentalRate>> pair = new Pair<Category, List<RentalRate>>(c, r);
+                res.add(pair);
+            }
+        }
+        return res;
+    }
+    
+    
 }
